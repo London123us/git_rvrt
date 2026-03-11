54f5429fc1f0299e22b1d0e53e462aadb8e4a337
London123us <neerajp898@gmail.com>
2026-03-11 10:16:01 +0530

    Done3

--- index.py
+++ index.py
@@ -12,11 +12,11 @@ outerfun()
 def outerfun2():
     print("Outer fun2")
 
-    innerfun2()
-    # It will give us error, cannot find out the innerfun2()
-    # because it's below then func call
+    # innerfun2()
+    # # It will give us error, cannot find out the innerfun2()
+    # # because it's below then func call
     def innerfun2():
         print("Inner fun2")
-    
+    innerfun2()
 
 outerfun2()
